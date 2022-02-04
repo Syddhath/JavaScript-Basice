14506edfda274b2842351c45a43e8ebfe6433095
Siddharth <siddharthgovindbhai.parmar@my.jcu.edu.au>
2022-02-04 15:33:33 +1100

    Landscape

---
+++ 0-Practice/SwapVariables.js
@@ -0,0 +1,21 @@
+// const colors = ["green"];
+// const btns = document.querySelectorAll(".btn")
+
+// btns.forEach(function (btn){
+//     btn.addEventListener("click", function(s){
+//         const background = s.currentTarget.classList;
+//         if(style.contains("a")){
+//             document.backgroundColor = colors[green];
+//         }
+//     })
+// })
+
+
+// let a = "red";
+// let b = "blue";
+// let c = a;
+// a = b;
+// b = c;
+
+// console.log(a);
+// console.log(b);
